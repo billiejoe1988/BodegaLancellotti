cdda6a22cbcfe685ad0752f091898ade72ab7078
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2024-04-08 22:49:19 -0300

    FIX, nav - footer

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -2,53 +2,9 @@ import React from "react";
 
 const Contacto = () => {
     return (
-        <main>
-            <section>
-                <h2>Informacion de Contacto</h2>
-                <div className="contact-item">
-                    <p>
-                        <span>
-                            <i className="fa fa-map-marker"></i> Direccion
-                        </span>
-                        {props.data ? props.data.direccion : "loading"}
-                    </p>
-                </div>
-                <div className="contact-item">
-                    <p>
-                        <span>
-                            <i className="fa fa-phone"></i> Telefono
-                        </span>{" "}
-                        {props.data ? props.data.telefono : "loading"}
-                    </p>
-                </div>
-                <div className="contact-item">
-                    <p>
-                        <span>
-                            <i className="fa fa-envelope-o"></i> Email
-                        </span>{" "}
-                        {props.data ? props.data.email : "loading"}
-                    </p>
-                </div>
-                <div className="col-md-12">
-                    <div className="row">
-                        <div className="social">
-                            <ul>
-                                <li>
-                                    <a href={props.data ? props.data.facebook : "/"}>
-                                        <i className="fa fa-facebook"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href={props.data ? props.data.instagram : "/"}>
-                                        <i className="fa fa-instagram"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </main>
+      <footer>
+        Contacto
+      </footer>
     );
 };
 
