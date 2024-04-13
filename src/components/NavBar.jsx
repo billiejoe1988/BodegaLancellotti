ba5353b8b1eac0c53888591d3e5397aacb7c12cb
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2024-04-13 19:42:12 -0300

    Update, link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Image, NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import Logo from '/logoB.png'; 
 import './navbar.css'
 
