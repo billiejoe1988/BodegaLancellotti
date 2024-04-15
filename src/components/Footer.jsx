1334db5b6098f0e88b3f4ae2df0fd98a9a4b0405
Leandro Carletti <carletti.l.n@gmail.com>
2024-04-15 18:34:01 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,53 +5,54 @@ import './footer.css'
 
 const Footer = () => {
     return (
-        <footer id="footer" style={{ backgroundImage: `url(${fondoFooter})`, backgroundSize: 'cover' }}>
-            <div className="footer-container">
-                <div className="contact-container card" style={{ backgroundColor: '#EADFCD', borderRadius: '10px', padding: '10px' }} >
-                    <div className="contact-item" >
-                        <h2>Contacto</h2>
-                        <p>
-                            <span>
-                                <i className="fa fa-map-marker"></i> Dirección:
-                            </span>
-                            Cuitiño 991 esquina, RN7, M5570 San Martín, Mendoza
-                        </p>
-                    </div>
-                    <div className="contact-item">
-                        <p>
-                            <span>
-                                <i className="fa fa-whatsapp"></i> WhatsApp:
-                            </span>
-                            <a href="https://wa.me/+542634419048">+54 2634419048</a>
-                        </p>
-                    </div>
-                    <div className="contact-item">
-                        <p>
-                            <span>
-                                <i className="fa fa-envelope-o"></i> Email:
-                            </span>
-                            <a href="mailto:bodega@zafiro.com">bodega@zafiro.com</a>
-                        </p>
-                    </div>
+        <footer id="footer" className="footer-container" style={{ backgroundImage: `url(${fondoFooter})`, backgroundSize: 'cover' }}>
+            <div className="tarjeta seccion-con-fondo-1" style={{ backgroundColor: '#EADFCD ', borderRadius: '10px', fontFamily: 'Libre Baskerville'}}>
+                <div className="contacto" style={{ fontFamily: 'Libre Baskerville' }}>
+                    <h1>CONTACTO</h1>
                 </div>
-                <section className="map-containercard" style={{ backgroundColor: '#EADFCD', borderRadius: '10px', padding: '12px', marginTop: '100px' }}>
-                    <h2>Ubicación</h2>
-                    <iframe
-                        title="Bodega lancellotti"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22484.304922252453!2d-68.37531794509552!3d-33.100882474332025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e5aca74cc9151%3A0x89d3017090764f11!2sLa%20Cautiva%20-%20Turismo%20Rural!5e0!3m2!1ses!2sar!4v1713104683886!5m2!1ses!2sar" 
-                        width="100%"
-                        height="200"
-                        style={{ border: 0, borderRadius: '5px' }}
-                        allowFullScreen=""
-                        loading="lazy"
-                    ></iframe>
-                </section>
-            </div>
-            <div className="logo-container">
-                <img src={logo} alt="Logo de la empresa" style={{ width: '200px', marginBottom: '10px' }} />
-            </div>
-            <div className="copyright">
-                <p className="copyright">&copy; 2024 Bodega Lancellotti. Todos los derechos reservados. Beber con moderación, prohibida su venta a menores de 18 años.</p>
+                <div className="footer-container">
+                    <div className="contact-container card" style={{ backgroundColor: '#EADFCD' }} >
+                        <div className="contact-item"  >
+                            <p>
+                                <span>
+                                    <i className="fa fa-map-marker"></i> Dirección:
+                                </span>
+                                Cuitiño 991 esquina, RN7, M5570 San Martín, Mendoza
+                            </p>
+                        </div>
+                        <div className="contact-item" style={{ backgroundColor: '#EADFCD' }} >
+                            <p>
+                                <span>
+                                    <i className="fa fa-whatsapp"></i> WhatsApp:
+                                </span>
+                                <a href="https://wa.me/+542634419048">+54 2634419048</a>
+                            </p>
+                        </div>
+                        <div className="contact-item" style={{ backgroundColor: '#EADFCD' }}>
+                            <p>
+                                <span>
+                                    <i className="fa fa-envelope-o"></i> Email:
+                                </span>
+                                <a href="mailto:bodega@zafiro.com">bodega@zafiro.com</a>
+                            </p>
+                        </div>
+                        <div className="copyright">
+                            <p className="copyright">&copy; 2024 Bodega Lancellotti - Mendoza, Argentina. Todos los derechos reservados. Beber con moderación, prohibida su venta a menores de 18 años.</p>
+                        </div>
+                    </div>
+                    <section className="map-containercard">
+                        <h2 style={{ color: '#31190D' }}>Ubicación</h2>
+                        <iframe
+                            title="Bodega lancellotti"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22484.304922252453!2d-68.37531794509552!3d-33.100882474332025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e5aca74cc9151%3A0x89d3017090764f11!2sLa%20Cautiva%20-%20Turismo%20Rural!5e0!3m2!1ses!2sar!4v1713104683886!5m2!1ses!2sar"
+                            width="100%"
+                            height="300"
+                            allowFullScreen=""
+                            loading="lazy"
+                        ></iframe>
+                    </section>
+                </div>
+
             </div>
         </footer>
     );
