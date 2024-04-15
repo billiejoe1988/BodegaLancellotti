4e05a798bc12b04f2e6f20d215abc9222a228e3c
Leandro Carletti <carletti.l.n@gmail.com>
2024-04-15 18:41:40 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer id="footer" className="footer-container" style={{ backgroundImage: `url(${fondoFooter})`, backgroundSize: 'cover' }}>
             <div className="tarjeta seccion-con-fondo-1" style={{ backgroundColor: '#EADFCD ', borderRadius: '10px', fontFamily: 'Libre Baskerville'}}>
                 <div className="contacto" style={{ fontFamily: 'Libre Baskerville' }}>
-                    <h1>CONTACTO</h1>
+                    <h1 style={{fontSize: '40px', fontWeight: 'bold'}}>Contacto</h1>
                 </div>
                 <div className="footer-container">
                     <div className="contact-container card" style={{ backgroundColor: '#EADFCD' }} >
@@ -46,7 +46,7 @@ const Footer = () => {
                             title="Bodega lancellotti"
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22484.304922252453!2d-68.37531794509552!3d-33.100882474332025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e5aca74cc9151%3A0x89d3017090764f11!2sLa%20Cautiva%20-%20Turismo%20Rural!5e0!3m2!1ses!2sar!4v1713104683886!5m2!1ses!2sar"
                             width="100%"
-                            height="300"
+                            height="335"
                             allowFullScreen=""
                             loading="lazy"
                         ></iframe>
